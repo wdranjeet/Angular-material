4d4bd5d3d30278db2b02eff610288f1dbe354b0c
wdranjeet <wdranjeet.m@gmail.com>
2020-07-15 22:41:19 +0530

    Initial Files and directory pushed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-form';
+export class AppComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  title = 'User Registration';
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+
+
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      userId: ['', Validators.required],
+      emailID: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      thirdCtrl: ['', Validators.required]
+    })
+  }
+
+  submitForm1() {
+   // console.log(this.firstFormGroup.value);
+  }
+  onSubmitForm(){
+    console.log('submitted');
+    console.log(this.firstFormGroup.get('userId'));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+
+
+    FormsModule,
+    ReactiveFormsModule,
+
+    MatStepperModule,
+    MatIconModule,
+    MatInputModule
   ],
-  providers: [],
+  providers: [
+    {
+    provide:  STEPPER_GLOBAL_OPTIONS,
+  useValue: { displayDefaultIndicatorType: false }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
